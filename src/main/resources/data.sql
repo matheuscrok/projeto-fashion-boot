3404cd54282cf0af9b047ec0e85f8ad68f8c04f7
MATHEUS DE CASTRO SOUSA <zenorhan@hotmail.com>
2021-04-17 01:29:40 -0300

    sql state category

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,16 @@
-INSERT INTO category VALUES (default,'roupa');
-INSERT INTO category VALUES (default,'roupa');
-INSERT INTO category VALUES (default,'roupa');
+INSERT INTO Category VALUES (default,'Roupas');
+INSERT INTO Category VALUES (default,'Calçados');
+INSERT INTO Category VALUES (default,'Acessórios');
+
+INSERT INTO State VALUES (default,'Goiás','GO');
+INSERT INTO State VALUES (default,'Minas Gerais','MG');
+INSERT INTO State VALUES (default,'São Paulo','SP');
+INSERT INTO State VALUES (default,'Bahia','BH');
+INSERT INTO State VALUES (default,'Paraná','PR');
+INSERT INTO State VALUES (default,'Santa Catarina','SC');
+INSERT INTO State VALUES (default,'Piauí','PI');
+INSERT INTO State VALUES (default,'Rio de Janeiro','RJ');
+INSERT INTO State VALUES (default,'Ceará','CE');
+INSERT INTO State VALUES (default,'Tocantins','TO');
+INSERT INTO State VALUES (default,'Alagoas','AL');
 
